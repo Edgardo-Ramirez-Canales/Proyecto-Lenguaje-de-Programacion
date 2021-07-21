2546f1e6c010faa6d92eb68cc11539cd7d24ec8c
Edgardo-Ramirez-Canales <60473889+Edgardo-Ramirez-Canales@users.noreply.github.com>
2021-07-21 06:25:57 -0300

    Update main.c

--- ANALIZADOR_LEXICO_M/main.c
+++ ANALIZADOR_LEXICO_M/main.c
@@ -25,6 +25,51 @@ char temp[2];
 enum TEstados{e0,e1,e2,e3,e4,e5,e6,e7,e8,e9,e10};
 enum TEstados Estado=e0;
 
+//FUNCION BUSCAS LAS PALABRAS RESERVADAS
+void Reservadas(){
+	for(int i=0;i<IndicadorTam;i++){
+		//strcmp COMPARA CARAACTER A CARACTER
+		//reservadas[i] son las palabras reservadas que declaramos como variable global al inicio del programa
+		
+  if(strcmp(reservadas[i],palabraIngresada)==0){
+			
+			if(strcmp(reservadas[0],palabraIngresada)==0)
+				contadorEncender++;
+			
+   if(strcmp(reservadas[1],palabraIngresada)==0)
+				contadorApagado++;
+			
+			
+			if(strcmp(reservadas[4],palabraIngresada)==0)
+				contadorCiclos++;
+				
+			
+			if(strcmp(reservadas[6],palabraIngresada)==0)
+				contadorCiclos++;
+			
+			
+			if(strcmp(reservadas[7],palabraIngresada)==0)
+				contadorCondicional++;
+			
+			contadorPalabrasReservadas++;
+			palabraIngresada[0]='\0';
+			break;
+		}
+		if(i==(IndicadorTam)-1){
+			exit(-1);
+		}
+	}		
+}
+
+/* Esta funcion se encarga se encarga de contar cuantas veces encontramos las palabras reservadas y
+para eso buscamos en el arreglo  [numero] ese es el que indica el numero de palabra que utlizamos en esa parte me falta ordenar 
+ese orden en las palabras
+*/
+
+
+
+
+
 int main() {
     printf("Hello, World!\n");
     printf("--------> ANALIZADOR LEXICO en C <------\n");
@@ -48,7 +93,7 @@ int main() {
     return 0;
 }
 /* FALTA ESTRUCTURAR LAS FUNCIONES EN ESTAS FUNCIONES ESTARAN LA LECTURA DEL ARCHIVO DE ENTRADA Y AQUI TAMBIEN HAREMOS
- * LOS CAMBIOS DE ESTADOS ASI COMO TAMBIEN TENDREMOS QUE TENER CONTADORES PARA MOSTRAR AL FINAL , TAMBIEN TENDREMOS QUE
+ * LOS CAMBIOS DE ESTADOS  , TAMBIEN TENDREMOS QUE
  * QUE COMPARAR LOS CARACTERES AQUI PIENSO QUE PODRIAMOS USAR FUNCIONES QUE ESTAN EN LA BIBLIOTECA O SEPRAR LA CADENAS CADA
  * PUNTO Y COMO QUE ENCONTREMOS*/
 
