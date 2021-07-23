7f1bf29dc7fbdfad70e26dfbcaef417408493528
Edgardo-Ramirez-Canales <60473889+Edgardo-Ramirez-Canales@users.noreply.github.com>
2021-07-23 19:44:34 -0300

    Update main.c

--- ANALIZADOR_LEXICO_M/main.c
+++ ANALIZADOR_LEXICO_M/main.c
@@ -119,6 +119,21 @@ int main() {
     }
     printf( "LEYENDO CON EXITO....\n" ) ;
 
+	/*Esta parte me servira para imprimir los led y el agua*/
+	for(int i=0; i<7; i++){
+		for(int k = 0; k<6-i; k++)
+		{
+			printf(" ");
+		}
+		
+		for(int k=0; k<i*2+1; k++){
+		
+			printf("*!");
+		}
+		printf("*");
+		
+		printf("\n");
+	}
 
 
 
